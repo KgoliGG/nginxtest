06b35f39a5a1e5146d7c40bc899f061e209fd59b
ShrijalKhayargoli <kgolis3292@gmail.com>
2024-03-18 17:00:31 +0545

    added index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Web Application</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Web Application</h1>
+    <p>This is a simple PHP web application that displays some content.</p>
+    <?php
+        // Display the current date and time
+        echo "<p>Current date and time: " . date("Y-m-d H:i:s") . "</p>";
+    ?>
+</body>
+</html>
