fabfe067cdb0a5d4b24a1f6f14ef9ab575e34864
KgoliGG <kgolis3292@gmail.com>
2024-03-19 11:46:14 +0545

    test

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 </head>
 <body>
     <h1>Welcome to My PHP Web Application</h1>
+    <h2> Heloo Word</h2>
     <p>This is a simple PHP web application that displays some content.</p>
     <?php
         // Display the current date and time
